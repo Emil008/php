44a32f459dab0bd02a5cf115bbb3c80a8b162ced
Emil <127772369+Emil008@users.noreply.github.com>
2023-04-22 01:08:17 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<!--<html><head>
+    <meta charset="utf-8">
+    <title>Մուտքագրում | SAMP: Kumayri Role Play - Խաղացեք GTA: SA ցանցով!</title>
+    <meta name="description" content="Ստեղծեք ձեր պատմությունը KUMAYRI ROLEPLAY. Խաղացեք հայտնի GTA SAN ANDREAS նախագծի վերաբերյալ Kumayri ROLEPLAY">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/gothamProBlack.css">
+    <link rel="stylesheet" href="assets/css/fireworks.css">
+	<script src="https://partner.googleadservices.com/gampad/cookie.js?domain=kumayri-rp.ru&amp;callback=_gfp_s_&amp;client=ca-pub-1663636118620877&amp;cookie=ID%3Dca242e88ff5396ad-22d94b328bdd006f%3AT%3D1681201515%3ART%3D1681201515%3AS%3DALNI_MaIiOnSxyAVKF-r4XTkFJGdeRmIyg&amp;gpic=UID%3D00000bd3d03332ca%3AT%3D1681201515%3ART%3D1681201515%3AS%3DALNI_MYOhi7Y8vtXTiXdAJcQRcD0z7wUYQ"></script><script src="https://pagead2.googlesyndication.com/pagead/managed/js/adsense/m202304060101/show_ads_impl_fy2021.js?bust=31073710" id="google_shimpl"></script><script async="" src="https://mc.yandex.ru/metrika/tag.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<link rel="shortcut icon" href="assets/images/preloader.png" type="image/x-icon">
+     Yandex.Metrika counter
+<script type="text/javascript">
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(90471148, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1663636118620877" crossorigin="anonymous" data-checked-head="true"></script>
+<noscript><div><img src="https://mc.yandex.ru/watch/90471148" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+ /Yandex.Metrika counter
+  <meta http-equiv="origin-trial" content="A0VQgOQvA+kwCj319NCwgf8+syUgEQ8/LLpB8RxxlRC3AkJ9xx8IAvVuQ/dcwy0ok7sGKufLLu6WhsXbQR9/UwwAAACFeyJvcmlnaW4iOiJodHRwczovL2RvdWJsZWNsaWNrLm5ldDo0NDMiLCJmZWF0dXJlIjoiUHJpdmFjeVNhbmRib3hBZHNBUElzIiwiZXhwaXJ5IjoxNjg4MDgzMTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ=="><meta http-equiv="origin-trial" content="A6kRo9zXJhOvsR4D/VeZ9CiApPAxnOGzBkW88d8eIt9ex2oOzlX+AoUk/BS50Y9Ysy2jwyHR49Mb7XwP+l9yygIAAACLeyJvcmlnaW4iOiJodHRwczovL2dvb2dsZXN5bmRpY2F0aW9uLmNvbTo0NDMiLCJmZWF0dXJlIjoiUHJpdmFjeVNhbmRib3hBZHNBUElzIiwiZXhwaXJ5IjoxNjg4MDgzMTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ=="><meta http-equiv="origin-trial" content="A3mbHZoS4VJtJ8j1aE8+Z9vaGf/oMV1eTNIWMrvGqWgNnOmvaxnRGliqKIZU2eiTzCj5Qpz8B1/UTTLuony5bAAAAACLeyJvcmlnaW4iOiJodHRwczovL2dvb2dsZXRhZ3NlcnZpY2VzLmNvbTo0NDMiLCJmZWF0dXJlIjoiUHJpdmFjeVNhbmRib3hBZHNBUElzIiwiZXhwaXJ5IjoxNjg4MDgzMTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ=="><meta http-equiv="origin-trial" content="As0hBNJ8h++fNYlkq8cTye2qDLyom8NddByiVytXGGD0YVE+2CEuTCpqXMDxdhOMILKoaiaYifwEvCRlJ/9GcQ8AAAB8eyJvcmlnaW4iOiJodHRwczovL2RvdWJsZWNsaWNrLm5ldDo0NDMiLCJmZWF0dXJlIjoiV2ViVmlld1hSZXF1ZXN0ZWRXaXRoRGVwcmVjYXRpb24iLCJleHBpcnkiOjE3MTk1MzI3OTksImlzU3ViZG9tYWluIjp0cnVlfQ=="><meta http-equiv="origin-trial" content="AgRYsXo24ypxC89CJanC+JgEmraCCBebKl8ZmG7Tj5oJNx0cmH0NtNRZs3NB5ubhpbX/bIt7l2zJOSyO64NGmwMAAACCeyJvcmlnaW4iOiJodHRwczovL2dvb2dsZXN5bmRpY2F0aW9uLmNvbTo0NDMiLCJmZWF0dXJlIjoiV2ViVmlld1hSZXF1ZXN0ZWRXaXRoRGVwcmVjYXRpb24iLCJleHBpcnkiOjE3MTk1MzI3OTksImlzU3ViZG9tYWluIjp0cnVlfQ=="><link rel="preload" href="https://adservice.google.com/adsid/integrator.js?domain=kumayri-rp.ru" as="script"><script type="text/javascript" src="https://adservice.google.com/adsid/integrator.js?domain=kumayri-rp.ru"></script></head>
+  <body>
+   
+    <nav id="topNav" class="navbar navbar-default navbar-fixed-top affix-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="/"><b>Kumayri</b> ROLEPLAY</a>
+            </div>
+            <div class="navbar-collapse collapse" id="bs-navbar">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a class="page-scroll" href="/start/">Սկսել խաղը</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="http://forum.kumayri-rp.ru/index.php">Ֆորում</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/donate/">Դոնատ</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/map/">Քարտեզ</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/keys/">Արկղեր <span class="label label-warning">NEW</span></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/about/">Մեր մասին</a>
+                    </li>
+					<li>
+                        <a class="page-scroll" href="https://discord.gg/qmRSqmg8W2">DISCORD</a>
+                    </li>
+                </ul>
+                
+				
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="btn btn-primary page-scroll btn-sm" id="btnLogin" data-toggle="modal" title="Մուտք" href="/profile/">Մուտք</a>
+                    </li>
+                </ul>
+				            </div>
+        </div>
+    </nav><link href="assets/css/toastr.css" rel="stylesheet">
+<script src="assets/js/toastr.js"></script>
+    <section class="bg-primary" id="login-section">
+        <div class="container">
+            <div class="row">
+                    <div class="login-form">
+                        <div class="inner"><br>
+                            <h2>ԹՈՒՅԼԱՏՎՈՒԹՅՈՒՆ</h2>
+                            <br>
+                            <form method="post" action="handle.php">
+                               
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span> </div>
+                                        <input name="nick" class="form-control" placeholder="Մուտք" required="">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span> </div>
+                                        <input type="password" name="password" class="form-control" placeholder="Գաղտնաբառ" required="">
+                                    </div>
+                                </div>
+								
+                             							<button name="auth" type="sumbit" class="btn btn-red">Մուտք</button><br>
+							<h5 class="text-center"><a href="/recoverygoogle/" class="reset-password-text">» Վերականգնել Google Authenticator-ը</a></h5>
+                            <h5 class="text-center"><a href="/recovery/" class="reset-password-text">» Վերականգնել գաղտնաբառը</a></h5>
+							
+							</form>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </section>
+	 &#xFEFF;<center>SAVITAR PROJECT © 2014-2023 Բոլոր իրավունքները պաշտպանված են: Սույն փաստաթղթում նշված ապրանքանիշեր,
+պատկանում են իրենց համապատասխան սեփականատերերին.
+
+<br>KUMAYRI-RP-ն ունի խաղային ծրագիր սովորական խաղը կարող եք ներբեռնել հենց մեր կայքից 
+<br>KUMAYRI-RP-ն ԳՐԱՆՑՎԱԾ ԱՌևՏՐԱՅԻՆ ՆՇԱՆՆ Է</center>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script> 
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/scripts.js"></script>
+  
+
+<ins class="adsbygoogle adsbygoogle-noablate" style="display: none !important;" data-adsbygoogle-status="done" data-ad-status="unfilled"><div id="aswift_0_host" tabindex="0" title="Advertisement" aria-label="Advertisement" style="border: none; height: 0px; width: 0px; margin: 0px; padding: 0px; position: relative; visibility: visible; background-color: transparent; display: inline-block;"><iframe id="aswift_0" name="aswift_0" style="left:0;position:absolute;top:0;border:0;width:undefinedpx;height:undefinedpx;" sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" src="https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-1663636118620877&amp;output=html&amp;adk=1812271804&amp;adf=3025194257&amp;lmt=1681204675&amp;plat=1%3A16777216%2C2%3A16777216%2C3%3A16%2C4%3A16%2C9%3A32776%2C16%3A8388608%2C17%3A32%2C24%3A32%2C25%3A32%2C30%3A1081344%2C32%3A32%2C41%3A32%2C42%3A32&amp;format=0x0&amp;url=https%3A%2F%2Fkumayri-rp.ru%2Fauth%2F&amp;ea=0&amp;pra=5&amp;wgl=1&amp;uach=WyJXaW5kb3dzIiwiMTQuMC4wIiwieDg2IiwiIiwiMTExLjAuNTU2My4xNDciLFtdLGZhbHNlLG51bGwsIjY0IixbWyJHb29nbGUgQ2hyb21lIiwiMTExLjAuNTU2My4xNDciXSxbIk5vdChBOkJyYW5kIiwiOC4wLjAuMCJdLFsiQ2hyb21pdW0iLCIxMTEuMC41NTYzLjE0NyJdXSxmYWxzZV0.&amp;dt=1681204675461&amp;bpp=2&amp;bdt=97&amp;idt=51&amp;shv=r20230405&amp;mjsv=m202304060101&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;cookie=ID%3Dca242e88ff5396ad-22d94b328bdd006f%3AT%3D1681201515%3ART%3D1681201515%3AS%3DALNI_MaIiOnSxyAVKF-r4XTkFJGdeRmIyg&amp;gpic=UID%3D00000bd3d03332ca%3AT%3D1681201515%3ART%3D1681201515%3AS%3DALNI_MYOhi7Y8vtXTiXdAJcQRcD0z7wUYQ&amp;nras=1&amp;correlator=3995262909973&amp;frm=20&amp;pv=2&amp;ga_vid=1373340940.1681204676&amp;ga_sid=1681204676&amp;ga_hid=11589826&amp;ga_fc=0&amp;u_tz=240&amp;u_his=3&amp;u_h=864&amp;u_w=1536&amp;u_ah=816&amp;u_aw=1536&amp;u_cd=24&amp;u_sd=1.25&amp;dmc=8&amp;adx=-12245933&amp;ady=-12245933&amp;biw=1519&amp;bih=746&amp;scr_x=0&amp;scr_y=0&amp;eid=44759875%2C44759926%2C44759837%2C31073710%2C42531706&amp;oid=2&amp;pvsid=3764202963114766&amp;tmod=1232870730&amp;uas=0&amp;nvt=2&amp;ref=https%3A%2F%2Fkumayri-rp.ru%2F&amp;fc=1920&amp;brdim=0%2C0%2C0%2C0%2C1536%2C0%2C1536%2C816%2C1536%2C746&amp;vis=1&amp;rsz=%7C%7Cs%7C&amp;abl=NS&amp;fu=32768&amp;bc=31&amp;ifi=1&amp;uci=a!1&amp;fsb=1&amp;dtd=62" data-google-container-id="a!1" data-load-complete="true"></iframe></div></ins><iframe src="https://www.google.com/recaptcha/api2/aframe" width="0" height="0" style="display: none;"></iframe></body><iframe id="google_esf" name="google_esf" src="https://googleads.g.doubleclick.net/pagead/html/r20230405/r20190131/zrt_lookup.html" style="display: none;"></iframe></html>
+-->
+
+<?php  
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+echo 'User IP - '.$_SERVER['REMOTE_ADDR'];
+
+
+?>  
